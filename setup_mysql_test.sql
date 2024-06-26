8d26447da5b923748196de4a760777ee4b71387c
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-06-26 21:40:36 +0200

    Do task 3-4, project 0x02, AirBnB_clone MySQL

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- script that prepares a MySQL server
+-- creates database hbnb_test_db
+-- creates user hbnb_test 
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;

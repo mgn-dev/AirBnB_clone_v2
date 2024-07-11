79b9620a4bbbc4b668daa0e2e7bae18d4bcf0631
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-07-11 17:26:20 +0200

    Do task 5, project 0x03. AirBnB clone - Deploy static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,88 +1,92 @@
-# Configures a web server for deployment of web_static.
+# Puppet manifest to set up web servers for the deployment of web_static
 
-# Nginx configuration file
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 https://th3-gr00t.tk;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
+# Ensure the NGINX package is installed
 package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-} ->
-
-file { '/data':
-  ensure  => 'directory'
-} ->
-
-file { '/data/web_static':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "Holberton School Puppet\n"
-} ->
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-} ->
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+  ensure => installed,
 }
 
-file { '/var/www':
-  ensure => 'directory'
-} ->
+# Ensure the required directories are created
+file { '/data/web_static/releases/test/':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+  require => Package['nginx'],
+}
 
-file { '/var/www/html':
-  ensure => 'directory'
-} ->
+file { '/data/web_static/shared/':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
+  require => Package['nginx'],
+}
 
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "Holberton School Nginx\n"
-} ->
+# Ensure the index.html file is created
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0644',
+  require => File['/data/web_static/releases/test/'],
+}
 
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-} ->
+# Ensure the symbolic link is created
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  require => File['/data/web_static/releases/test/index.html'],
+}
 
+# Ensure the ownership of the /data directory
+file { '/data/':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  recurse => true,
+  require => File['/data/web_static/releases/test/', '/data/web_static/shared/'],
+}
+
+# Update the Nginx configuration to service specified content
 file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => $nginx_conf
-} ->
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  require => Package['nginx'],
+  content => @("EOF")
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
 
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+}
+| EOF
+}
+
+# Ensure the Nginx service is running and enabled
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-available/default'],
 }

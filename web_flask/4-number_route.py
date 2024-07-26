9592c85008932665083fe46f90d451674489bb71
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-07-26 22:21:51 +0200

    Do task 4, project 0x04. AirBnB clone - Web framework

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,5 +29,10 @@ def python_text(text):
     return f"Python {text}"
 
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    return f"{n} is a number"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

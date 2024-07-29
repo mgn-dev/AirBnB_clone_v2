6ad5ac2f667f3d067367631ce4631efd3c3c89b0
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-07-29 23:34:34 +0200

    Do task 2, 0x02. AirBnB clone - MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,16 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
+    def all(self):
         """Returns a dictionary of models currently in storage"""
-        if cls:
-            filtered = {}
-            for k, v in self.__objects.items():
-                if v.__class__ == cls:
-                    filtered[k] = v
-            return filtered
-
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -55,14 +48,3 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """ delete an existing element
-        """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
-
-    def close(self):
-        """deserializes JSON file to objects"""
-        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.city import City
-from models import storage
-
-from base_model import Base, Column, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state")
-
-    @property
-    def cities(self):
-        """getter for list of city instances related to the state"""
-        city_list = []
-        all_cities = storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
-
-# class State(BaseModel):
-#     """ State class """
-#     name = ""
+    name = ""
